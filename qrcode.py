a28890fa73cd7a72835d6fb61d06df24820337ea
Jeevan <57611618+jeevandowney@users.noreply.github.com>
2020-05-26 17:52:42 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,4 +1,4 @@
 import pyqrcode
-url = pyqrcode.create("www.instagram.com")
+url = pyqrcode.create("ENTER THE URL YOU REQUIRED")
 url.svg('uca-url . svg', scale = 8)
 print(url.terminal(quiet_zone=1))
